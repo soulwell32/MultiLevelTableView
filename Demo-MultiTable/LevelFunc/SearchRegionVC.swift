2ef462fe90153503e3fd9de84f0c1610893b5eb6
Meilin <meilin.gtd@foxmail.com>
2019-01-11 17:28:53 +0800

    Search页面

---
+++ Demo-MultiTable/LevelFunc/SearchRegionVC.swift
@@ -0,0 +1,35 @@
+//
+//  SearchRegionVC.swift
+//  Demo-MultiTable
+//
+//  Created by 梅霖 on 2019/1/11.
+//  Copyright © 2019 梅霖. All rights reserved.
+//
+
+import UIKit
+
+class SearchRegionVC: UIViewController {
+
+    
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var searchTextF: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Demo-MultiTable/LevelFunc/View/JKSearchHistoryView.swift
@@ -0,0 +1,15 @@
+//
+//  JKSearchHistoryView.swift
+//  Demo-MultiTable
+//
+//  Created by 梅霖 on 2019/1/11.
+//  Copyright © 2019 梅霖. All rights reserved.
+//
+
+import UIKit
+
+class JKSearchHistoryView: UITableView {
+
+    
+
+}
